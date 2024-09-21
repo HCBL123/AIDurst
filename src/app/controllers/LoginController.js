774cd35cbc874eb82414052df50f897d2dc4f61b
HCBL123 <baolongben2008@gmail.com>
2024-09-21 10:11:55 +0700

    update bycript

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -1,5 +1,6 @@
 // Doctor models to access data from mongodb
 const Doctors = require("../models/Doctors");
+const bcrypt = require('bcryptjs');
 // FUNCTION to convert scheme to object
 const { multipleMongooseToObject } = require("../../util/mongoose");
 const { mongooseToObject } = require("../../util/mongoose");
@@ -28,18 +29,23 @@ class LoginController {
     if (!user) {
       return res.status(400).json({ message: "Invalid Credentials" });
     } else {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Invalid Credentials' });
+      }
       req.session.isLoggedIn = true;
       req.session.username = usr;
 
       res.redirect("/profile/" + usr);
     }
   }
+
   async logout(req, res, next) {
     await req.session.destroy((err) => {
       if (err) {
         console.log(err);
       } else {
-        res.redirect("/results");
+        res.redirect("/home");
       }
     });
   }
